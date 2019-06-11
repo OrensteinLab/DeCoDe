a762cddf1488b1afd0765ac5f85780db17d2dc8c
Tyler Shimko <TShimko@gmail.com>
2019-06-11 05:35:04 -0700

    Enable time limit in CLI

--- run_ilp.py
+++ run_ilp.py
@@ -7,31 +7,33 @@ import json
 
 @click.command()
 @click.argument('alignment_file', required=True, type=click.Path(exists=True, dir_okay=False, readable=True))
+@click.argument('output_file', required=True, type=click.Path(exists=False, dir_okay=False, readable=True))
 @click.option('--limit', required=True, type=int, prompt='Total lib size limit', help='Total library size limit.')
 @click.option('--sublib', required=True, type=int, prompt='Sublibrary limit', help='Total sublibrary limit')
-@click.option('--codon_table', default=None, type=click.File('r'), help='Specify an alternate codon table.')
 @click.option('--bins', default=100, show_default=True, help='Specify the number of bins for approximation of multi-sublibrary optimizations.')
-@click.option('--solver', default='gurobi', show_default=True, type=click.Choice(['gurobi', 'glpk']), help='Backend solver to use.')
-@click.option('--time_limit', default=0, show_default=True, help='Time limit for the ILP solver.')
-@click.option('--approx', is_flag=True, default=False, show_default=True, help='Run ILP with approximations for a single oligo.')
-@click.option('-v', '--verbose', is_flag=True, default=False, show_default=True, help='Display solver output.')
-@click.option('-p', '--parallel', is_flag=True, default=True, show_default=True, help='Run solver in parallel mode.')
+@click.option('--time-limit', default=0, show_default=True, help='Time limit in seconds for the ILP solver.')
+@click.option('--threads', default=0, show_default=True, help='Time limit in seconds for the ILP solver.')
 @click.option('-q', '--quiet', is_flag=True, default=False, show_default=True, help='Run quietly.')
 
-# solve_library(S, lib_lim, n_oligos, A=codon_table, bins=bins, solver='gurobi', verbose=True, parallel=True, approximate=False)
 
-def optimize_lib(alignment_file, limit, sublib, codon_table, bins, solver, verbose, parallel, approx, quiet, time_limit):
-    """Optimize a redundent codon library given a set of target sequences, a total size limit, and a sublibrary count limit."""
+def optimize_lib(alignment_file, output_file, limit, sublib, bins, time_limit, threads, quiet):
+    """Optimize a degenerate codon library given a set of target sequences, a total size limit, and a sublibrary count limit."""
+    
+    if quiet:
+        verbose = False
+    else: verbose=True
     
     if not quiet:
         click.echo('Reading MSA file...')
     
+    # Read the MSA file
     fixed_positions, variable_positions, sequences = process_msa(alignment_file, 'clustal')
     
     if not quiet:
         click.echo('Removing fixed positions...')
     
-    n_targets, n_var_pos, S = create_S(sequences)
+    # Generate the one-hot encoded input
+    n_targets, n_var_pos, O = create_O(sequences)
     
     if not quiet:
         click.echo('')
@@ -41,22 +43,21 @@ def optimize_lib(alignment_file, limit, sublib, codon_table, bins, solver, verbo
         click.echo('Number of variable positions:\t{}'.format(n_var_pos))
         click.echo('')
     
-    if not quiet:
-        click.echo('Solving for degenerate codon library with {}...\n'.format(solver))
-    
+    # Start timing
     start = time.time()
     
-    solution = solve_library(S, limit, sublib, A=codon_table, bins=bins,
-                             solver=solver, verbose=verbose, parallel=parallel,
-                             approximate=approx, time_limit=time_limit)
+    # Get the ILP solver solution
+    solution = solve_library(O, limit, sublib, bins=bins, verbose=verbose, time_limit=time_limit, threads=threads)
     
+    # End timing
     end = time.time()
     
+    # Extract setup and solve times
     total_time = end - start
     solve_time = solution['problem'].solver_stats.solve_time
     construct_time = total_time - solve_time
     
-    
+    # Get library stats
     n_covered = int(np.sum(solution['binary_coverage']))
     codons = retrieve_codons(solution['codon_selection'])
     total_lib_size = int(calc_num_seqs(codons))
@@ -69,11 +70,14 @@ def optimize_lib(alignment_file, limit, sublib, codon_table, bins, solver, verbo
         click.echo('Probability on target:\t\t{:0.5f}'.format(on_target_p))
         click.echo('')
     
-    parsed_lib = parse_lib(fixed_positions, codons)
+    # Parse the library
+    total_seq_length = len(fixed_positions) + len(variable_positions)
+    parsed_lib = parse_lib(total_seq_length, fixed_positions, codons)
     
     if not quiet:
         click.echo('Writing output...')
     
+    # Generate and write the output
     data = {
         'fixed_positions': fixed_positions,
         'variable_positions': variable_positions,
@@ -89,7 +93,7 @@ def optimize_lib(alignment_file, limit, sublib, codon_table, bins, solver, verbo
         'total_time': total_time
     }
     
-    with open('ILP_lib_opt_{:d}_{:d}.json'.format(limit, sublib), 'w') as fp:
+    with open(output_file, 'w') as fp:
         json.dump(data, fp)
     
 if __name__ == '__main__':
